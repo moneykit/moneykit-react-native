0902e72238f0cad4dfc1bb344362ccaa4e4771cc
Ethan Lowry <ethan@moneykit.com>
2023-11-07 17:09:51 +0000

    Wire ios success/exit callbacks

--- example/App.tsx
+++ example/App.tsx
@@ -1,19 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ConnectConfiguration,
+  presentInstitutionSelectionFlow,
+} from "moneykit-connect-react-native-source";
+import { StyleSheet, View } from "react-native";
 
-import * as Connect from 'moneykit-connect-react-native-source';
-import Button from './Button';
+import Button from "./Button";
+
+const config: ConnectConfiguration = {
+  linkSessionToken:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IlB3Y2VfUGFfUV9jSWhkLTFWWlNGMjlzS3E4aWNfQmJYZTJJcG93Q3JaUDQiLCJlbnYiOiJzYW5kYm94IiwibW9kZSI6InNhbmRib3giLCJsaW5rX2lkIjpudWxsLCJpbnN0aXR1dGlvbl9pZCI6bnVsbCwiaW5zdGl0dXRpb25fY29sb3IiOm51bGwsImluc3RpdHV0aW9uX25hbWUiOm51bGwsImluc3RpdHV0aW9uX2F2YXRhciI6bnVsbCwiYXBwX25hbWUiOiJNb25leUtpdCBXZWIgRGVtbyIsInJlZGlyZWN0X3VyaSI6Imh0dHBzOi8vZGVtby5zdGFnZS51ZTIubW9uZXlraXQuY29tL3JlZGlyZWN0In0.eyJzdWIiOiJzYW5kYm94XzFhMTQxNWM0LTg3NDItNDM0Zi1hOWVlLTA3YTRhNWExOTk4NyIsImF1ZCI6WyJodHRwczovL2FwaS5zdGFnZS51ZTIubW9uZXlraXQuY29tL2xpbmstc2Vzc2lvbiJdLCJjdXN0b21lcl9hcHBfaWQiOiIxM2Q1ZmNiZS03NGI3LTQ4OTktYThjMi1mYmZkYTJkMjk4OGMiLCJpc3MiOiJodHRwczovL2FwaS5zdGFnZS51ZTIubW9uZXlraXQuY29tIiwiZXhwIjoxNjk5Mzc3NzQxLCJpYXQiOjE2OTkzNzU5NDF9.iwAdKRkddHWWgizWzF5ZSLrNFHBQX7ke5YzseeLb_3s",
+  onSuccess(payload) {
+    console.log("Success payload: ", payload);
+  },
+  onExit() {
+    console.log("Exited");
+  },
+};
+
+const presentConnectInstitutionSelectionFlow = () => {
+  presentInstitutionSelectionFlow(config);
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Button
         title="Connect a bank"
-        onPress={() => {
-          Connect.presentInstitutionSelectionFlow({
-            "linkSessionToken": ""
-          })
-          }
-        }
+        onPress={presentConnectInstitutionSelectionFlow}
       />
     </View>
   );
@@ -22,15 +35,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 16,
     lineHeight: 20,
-    fontWeight: 'normal',
+    fontWeight: "normal",
     letterSpacing: 0.25,
-    color: 'black',
+    color: "black",
   },
 });
--- src/Connect.types.ts
+++ src/Connect.types.ts
@@ -1,3 +1,42 @@
-export type Configuration = {
+export type ConnectConfiguration = {
   linkSessionToken: string;
+  onSuccess: (payload: SuccessPayload) => void;
+  onExit: () => void;
+};
+
+export type SuccessPayload = {
+  institution: Institution;
+  accounts: Account[];
+  token: ExchangeableToken | null;
+  trackedScreens: TrackedScreen[];
+};
+
+export type ExchangeableToken = {
+  value: string;
+};
+
+export type TrackedScreen = {
+  name: string;
+  duration: number;
+  requestTime: number | null;
+};
+
+export type Account = {
+  id: string;
+  name: string;
+  type: string;
+  mask: string | null;
+};
+
+export type Institution = {
+  id: string;
+  name: string;
+  country: string;
+  domain: string | null;
+  color: string;
+  colorDark: string | null;
+  avatar: string;
+  avatarDark: string | null;
+  logo: string | null;
+  logoDark: string | null;
 };
