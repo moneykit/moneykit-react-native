4fb2c730f83169b4ec1292064c31b6b8f13cea0b
Niall Smyth <niall.smyth@gmail.com>
2023-11-20 14:03:21 +0000

    Android SDK Integration (#4)
    * Build with SDK 34, minimum 26 for MK Connect android SDK
    
    * Add MK Connect Android SDK dependency
    
    * Add code to interface between RN JS layer and the Android SDK

--- android/src/main/java/expo/modules/moneykitconnectreactnativesource/ConnectModule.kt
+++ android/src/main/java/expo/modules/moneykitconnectreactnativesource/ConnectModule.kt
@@ -1,9 +1,35 @@
 package expo.modules.moneykitconnectreactnativesource
 
+import android.net.Uri
+import com.moneykit.connect.MkConfiguration
+import com.moneykit.connect.MkLinkHandler
+import com.moneykit.connect.entities.MkLinkError
+import com.moneykit.connect.entities.MkLinkEvent
+import com.moneykit.connect.entities.MkLinkSuccessType
+import com.moneykit.connect.entities.MkLinkedInstitution
+import com.moneykit.connect.entities.MkRelinkedInstitution
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.records.Field
+import expo.modules.kotlin.records.Record
+
+class Configuration: Record {
+  @Field
+  var linkSessionToken: String = ""
+}
 
 class ConnectModule : Module() {
+  private val currentActivity
+    get() = appContext.currentActivity ?: throw Exceptions.MissingActivity()
+
+  private var linkHandler: MkLinkHandler? = null
+
+  private val onSuccess = "onSuccess"
+  private val onEvent = "onEvent"
+  private val onExit = "onExit"
+
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -13,35 +39,129 @@ class ConnectModule : Module() {
     // The module will be accessible from `requireNativeModule('Connect')` in JavaScript.
     Name("Connect")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
+    // Defines event names that the module can send to JavaScript.
+    Events(
+      "onSuccess",
+      "onEvent",
+      "onExit",
     )
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+    AsyncFunction("presentInstitutionSelectionFlow") { config: Configuration ->
+      linkHandler = createLinkHandler(config.linkSessionToken)
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
+      linkHandler?.presentInstitutionSelectionFlow(currentActivity)
+    }.runOnQueue(Queues.MAIN)
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
+    AsyncFunction("presentLinkFlow") { config: Configuration ->
+      linkHandler = createLinkHandler(config.linkSessionToken)
+
+      linkHandler?.presentInstitutionSelectionFlow(currentActivity)
+    }.runOnQueue(Queues.MAIN)
+
+    AsyncFunction("continueFlow") { urlString: String ->
+      val url = Uri.parse(urlString)
+      linkHandler?.continueFlow(url)
+    }.runOnQueue(Queues.MAIN)
+  }
+
+  private fun createLinkHandler(linkSessionToken: String): MkLinkHandler? {
+    try {
+      val configuration = MkConfiguration(
+        sessionToken = linkSessionToken,
+        onSuccess = { handleConnectSuccess(it) },
+        onExit = { handleConnectExit(null) },
+        onEvent = { handleConnectEvent(it) }
+      )
+
+      return MkLinkHandler(configuration)
+    } catch (error: MkConfiguration.ConfigurationError) {
+      sendEvent(onExit, mapOf(
+//      "identifier" to error.identifier,
+        "displayedMessage" to error.message,
+//        "requestId" to error.requestId
       ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ConnectView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ConnectView, prop: String ->
-        println(prop)
-      }
+      return null
+    } catch (error: Exception) {
+      sendEvent(onExit, mapOf(
+//      "identifier" to error.identifier,
+        "displayedMessage" to error.message,
+//        "requestId" to error.requestId
+      ))
+      return null
     }
   }
+
+  private fun handleConnectSuccess(successType: MkLinkSuccessType) {
+    when (successType) {
+      is MkLinkSuccessType.Linked ->
+        sendEvent(onSuccess, successType.institution.toMap())
+
+      is MkLinkSuccessType.Relinked ->
+        sendEvent(onSuccess, successType.institution.toMap())
+    }
+  }
+
+  private fun handleConnectExit(error: MkLinkError?) {
+    if (error == null) {
+      sendEvent(onExit)
+      return
+    }
+
+    sendEvent(onExit, mapOf(
+//      "identifier" to error.identifier,
+      "displayedMessage" to error.displayedMessage,
+      "requestId" to error.requestId
+    ))
+  }
+
+  private fun handleConnectEvent(event: MkLinkEvent) {
+    sendEvent(onEvent, mapOf(
+      "name" to event.name,
+      "sessionId" to event.sessionId,
+      "properties" to event.properties
+    ))
+  }
+
+  private fun MkLinkedInstitution.toMap() = mapOf(
+    "institution" to mapOf(
+      "id" to institution.id,
+      "name" to institution.name,
+    ),
+    "token" to token.value,
+    "accounts" to accounts.map { account ->
+      mapOf(
+        "id" to account.id,
+        "name" to account.name,
+        "mask" to account.mask,
+        "type" to account.type,
+      )
+    },
+    "trackedScreens" to trackedScreens.map { trackedScreen ->
+      mapOf(
+        "name" to trackedScreen.name,
+        "tag" to trackedScreen.tag,
+      )
+    },
+  )
+
+  private fun MkRelinkedInstitution.toMap() = mapOf(
+    "institution" to mapOf(
+      "id" to institution.id,
+      "name" to institution.name,
+    ),
+    "accounts" to accounts.map { account ->
+      mapOf(
+        "id" to account.id,
+        "name" to account.name,
+        "mask" to account.mask,
+        "type" to account.type,
+      )
+    },
+    "trackedScreens" to trackedScreens.map { trackedScreen ->
+      mapOf(
+        "name" to trackedScreen.name,
+        "tag" to trackedScreen.tag,
+      )
+    },
+  )
 }
